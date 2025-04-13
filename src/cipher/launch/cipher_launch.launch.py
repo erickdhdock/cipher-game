4fb2ccd77576978a4a84744a870ab303211f3cdc
Eric Kang <erickdhdock@gmail.com>
2025-04-13 14:52:01 +1000

    ?

--- src/cipher/launch/cipher_launch.launch.py
+++ src/cipher/launch/cipher_launch.launch.py
@@ -5,12 +5,12 @@ from launch_ros.actions import Node
 def generate_launch_description():
    return LaunchDescription([
         Node(
-            package='cipher_game',
+            package='cipher',
             executable='cipher_server_node',
             name='cipher_server',
         ),
         Node(
-            package='cipher_game',
+            package='cipher',
             executable='decipher_node',
             name='decipher',
         )
